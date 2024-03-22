184d0845561083a449c752b94515be24924621d4
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2024-03-22 19:30:52 +0200

    ..

--- js/navbar.js
+++ js/navbar.js
@@ -1,52 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var isLoggedIn = localStorage.getItem("loggedIn") === "true";
-  var logoutBtn = document.querySelector(".btn-logout");
-
+ 
   var parentLocation = window.location.href;
   var navbarLinks = document.querySelectorAll(".navbar-link");
-  var indexLink = document.querySelector('.navbar-link[href="../index.html"]');
-  var signUpBtn = document.querySelector(".btn-dark");
-  var signInBtn = document.querySelector(".btn-outline-dark");
-
-  const hideButtons = () => {
-    signUpBtn.style.display = "none";
-    signInBtn.style.display = "none";
-    logoutBtn.style.display = "block";
-  };
-
-  const showButtons = () => {
-    signUpBtn.style.display = "block";
-    signInBtn.style.display = "block";
-    logoutBtn.style.display = "none";
-  };
-
-  if (isLoggedIn) {
-    hideButtons();
-  } else {
-    showButtons();
-    window.parent.location.href = "../pages/login.html";
-  }
-
-  logoutBtn.addEventListener("click", () => {
-    localStorage.removeItem("loggedIn");
-    window.parent.location.href = "../pages/login.html";
-  });
-
-  signUpBtn.addEventListener("click", () => {
-    try {
-      window.parent.location.href = "../pages/registration.html";
-    } catch (e) {
-      window.location.href = "../pages/registration.html";
-    }
-  });
-
-  signInBtn.addEventListener("click", () => {
-    try {
-      window.parent.location.href = "../pages/login.html";
-    } catch (e) {
-      window.location.href = "../pages/login.html";
-    }
-  });
+  
 
   navbarLinks.forEach(function (link) {
     var linkPath = link.getAttribute("href");
